5dede763fff06ea0c4084bd636607bb786696a0a
Olli <oparviai@iki.fi>
2025-02-23 12:54:41 +0200

    Refactor pow() usage
    Refactor pow() usage:
    - use base of 0.5^k instead of 1/(2^k)
    - skip pow if using INTEGER_SAMPLES

--- source/SoundTouch/FIRFilter.cpp
+++ source/SoundTouch/FIRFilter.cpp
@@ -56,7 +56,6 @@ using namespace soundtouch;
 FIRFilter::FIRFilter()
 {
     resultDivFactor = 0;
-    resultDivider = 0;
     length = 0;
     lengthDiv8 = 0;
     filterCoeffs = nullptr;
@@ -207,24 +206,24 @@ void FIRFilter::setCoefficients(const SAMPLETYPE *coeffs, uint newLength, uint u
     assert(newLength > 0);
     if (newLength % 8) ST_THROW_RT_ERROR("FIR filter length not divisible by 8");
 
-    #ifdef SOUNDTOUCH_FLOAT_SAMPLES
-        // scale coefficients already here if using floating samples
-        double scale = 1.0 / resultDivider;
-    #else
-        short scale = 1;
-    #endif
-
     lengthDiv8 = newLength / 8;
     length = lengthDiv8 * 8;
     assert(length == newLength);
 
     resultDivFactor = uResultDivFactor;
-    resultDivider = (SAMPLETYPE)::pow(2.0, (int)resultDivFactor);
 
     delete[] filterCoeffs;
     filterCoeffs = new SAMPLETYPE[length];
     delete[] filterCoeffsStereo;
     filterCoeffsStereo = new SAMPLETYPE[length*2];
+
+#ifdef SOUNDTOUCH_FLOAT_SAMPLES
+    // scale coefficients already here if using floating samples
+    const double scale = ::pow(0.5, (int)resultDivFactor);;
+#else
+    const short scale = 1;
+#endif
+
     for (uint i = 0; i < length; i ++)
     {
         filterCoeffs[i] = (SAMPLETYPE)(coeffs[i] * scale);
